0f02a63c2cf30edb35ec46468c15a428fb507226
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-05 11:14:58 -0400

    Able to delete a customer

--- src/main/java/com/stradtkt/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/stradtkt/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.stradtkt.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,15 @@ public class CustomerRestController {
 	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		customerService.saveCustomer(theCustomer);
 		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		if(tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found: " + customerId);
+		}
+		customerService.deleteCustomer(customerId);
+		return "Deleted customer with the id of: " + customerId;
 	}
 }
